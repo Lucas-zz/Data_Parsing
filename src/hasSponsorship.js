a0f2ce48bb86d9b44c8cc1044a69be2f4399b317
Lucas <lucasazzollinivieira@gmail.com>
2022-04-22 20:45:52 -0300

    chores: changes to what data to get from db

--- src/hasSponsorship.js
+++ src/hasSponsorship.js
@@ -1,25 +1,30 @@
-import pg from 'pg';
+import { connection } from './database.js';
 import fs from 'fs';
-
-const { Pool } = pg;
-
-const connection = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    database: 'parsing',
-    password: 'lucas'
-});
 
 const hasSponsorship = true;
 
-const query = connection.query('SELECT * FROM repositories WHERE "hasSponsorship"=$1', [hasSponsorship]);
+const query = connection.query(`
+    SELECT 
+        name,
+        owner,
+        description,
+        topic,
+        language,
+        stars
+    FROM 
+        repositories
+    WHERE
+        "hasSponsorship" = $1
+    ORDER BY
+        stars DESC
+    `, [hasSponsorship]
+);
 
 query.then(result => {
     const filename = "sponsored-repos.json";
     const myJSON = JSON.stringify(result.rows);
 
-    fs.writeFileSync(filename, myJSON);
+    fs.writeFileSync(`./createdFiles/${filename}`, myJSON);
 
     console.log("done");
 });
